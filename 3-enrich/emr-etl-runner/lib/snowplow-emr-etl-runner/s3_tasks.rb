33976d0f939751965c9cb20dabc080728bad5cfb
Rupesh Mane <rupeshmane@hotmail.com>
2014-11-25 10:36:09 -0800

    Fix extension for input log file.

--- 3-enrich/emr-etl-runner/lib/snowplow-emr-etl-runner/s3_tasks.rb
+++ 3-enrich/emr-etl-runner/lib/snowplow-emr-etl-runner/s3_tasks.rb
@@ -56,8 +56,9 @@ module Snowplow
         files_to_move = case
         when (args[:start].nil? and args[:end].nil?)
           if config[:etl][:collector_format] == 'clj-tomcat'
-            '.*localhost\_access\_log\.txt\-.*'
-          else
+            #'.*localhost\_access\_log\.txt\-.*'
+            '.*localhost\_access\_log\.txt.*'
+           else
             '.+'
           end
         when args[:start].nil?
